e7bd95810bdf0130fb6e0e9485719fbd55500998
derrick <derrick@toontownrewritten.com>
2023-03-27 22:17:28 -0400

    feat: add ability to update item quantity in cart

--- frontend/src/features/cart/cartSlice.tsx
+++ frontend/src/features/cart/cartSlice.tsx
@@ -1,8 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
+import { Product } from '../../types/types';
 
-interface CartProduct {
-  id: string;
+interface CartProduct extends Product {
   cartTotalItems: number;
 }
 
@@ -30,7 +30,6 @@ const cartSlice = createSlice({
       );
       if (productIndex >= 0) {
         state.cartProducts[productIndex].cartTotalItems += 1;
-        console.log(state.cartProducts[productIndex].cartTotalItems);
         toast.success(
           `Added "${action.payload.title}" to cart! (Currently ${state.cartProducts[productIndex].cartTotalItems})`
         );
@@ -40,11 +39,38 @@ const cartSlice = createSlice({
         toast.success(`Added "${action.payload.title}" to cart!`);
       }
       localStorage.setItem('cartProducts', JSON.stringify(state.cartProducts));
-      // console.log(state.cartProducts);
-      // console.log(state.cartTotalItems);
     },
+    decreaseQuantity(state, action) {
+      const productIndex = state.cartProducts.findIndex(
+        item => item.id === action.payload.id
+      );
+      if (state.cartProducts[productIndex].cartTotalItems > 1) {
+        state.cartProducts[productIndex].cartTotalItems -= 1;
+        toast.success(
+          `Decreased "${action.payload.title}" from cart! (Currently ${state.cartProducts[productIndex].cartTotalItems})`
+        );
+      } else {
+        state.cartProducts.splice(productIndex, 1);
+        toast.success(`Removed "${action.payload.title}" from cart!`);
+      }
+      localStorage.setItem('cartProducts', JSON.stringify(state.cartProducts));
+    },
+    deleteItem(state, action) {
+      const productIndex = state.cartProducts.findIndex(
+        item => item.id === action.payload.id
+      );
+      state.cartProducts.splice(productIndex, 1);
+      localStorage.setItem('cartProducts', JSON.stringify(state.cartProducts));
+    },
+    clearCart(state) {
+      state.cartProducts = [];
+      localStorage.setItem('cartProducts', JSON.stringify(state.cartProducts));
+      toast.success('Cart emptied!');
+    },
+    subtotal(state, action) {},
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, decreaseQuantity, clearCart, deleteItem } =
+  cartSlice.actions;
 export default cartSlice.reducer;
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,10 +1,85 @@
 import { useTitle } from '../hooks/useTitle';
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../app/hooks';
+import './Cart.scss';
+
+import {
+  addToCart,
+  decreaseQuantity,
+  clearCart,
+  deleteItem,
+} from '../features/cart/cartSlice';
+import { useAppDispatch } from '../app/hooks';
 
 export default function Cart() {
-  useTitle('Cart | Knock');
+  const cart = useAppSelector(state => state.cart);
+
+  const dispatch = useAppDispatch();
+
+  const handleAddToCart = (product: any) => {
+    dispatch(addToCart(product));
+  };
+
+  const handleDecreaseQuantity = (product: any) => {
+    dispatch(decreaseQuantity(product));
+  };
+
+  const handleClearCart = (product: any) => {
+    dispatch(clearCart(product));
+  };
+
+  const handleDeleteItem = (product: any) => {
+    dispatch(deleteItem(product));
+  };
+
+  useTitle('Cart | Knock.app');
   return (
     <>
-      <div>Cart</div>
+      <div className='cart__header'>
+        <h1>Cart</h1>
+        <button onClick={() => handleClearCart(cart)}>Remove</button>
+      </div>
+      <div className='cart__headings'>
+        <h2>PRODUCT</h2>
+        <h2>QUANTITY</h2>
+        <h2>PRICE</h2>
+      </div>
+      <div className='cart__products'>
+        {cart.cartProducts?.map(cartProduct => (
+          <div key={cartProduct.id} className='cart__product'>
+            <section className='cart__details'>
+              <img
+                className='cart__thumbnail'
+                src={cartProduct.image}
+                alt={cartProduct.title}
+              />
+              <h3>{cartProduct.title}</h3>
+              <span>size</span>
+            </section>
+            <section>
+              <div>
+                <button onClick={() => handleDecreaseQuantity(cartProduct)}>
+                  -
+                </button>
+                <span>{cartProduct.cartTotalItems}</span>
+                <button onClick={() => handleAddToCart(cartProduct)}>+</button>
+              </div>
+              <div>
+                <button onClick={() => handleDeleteItem(cartProduct)}>
+                  Remove
+                </button>
+              </div>
+            </section>
+            <section>
+              <p>Total :${cartProduct.price * cartProduct.cartTotalItems}</p>
+              <p>
+                <em>${cartProduct.price} per item</em>
+              </p>
+            </section>
+          </div>
+        ))}
+      </div>
+      <span>Subtotal: </span>
     </>
   );
 }
